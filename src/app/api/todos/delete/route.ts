ba52c727e2c7573ea5c82344f68e9d2a744f0d34
lekan.dev <realjoy9@gmail.com>
2024-07-24 16:58:42 +0100

    delete

---
+++ src/app/api/todos/delete/route.ts
@@ -0,0 +1,13 @@
+import { pusherServer } from "@/lib/pusher";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { id } = await request.json();
+    pusherServer.trigger("private-todo-channel", "delete", { id });
+    return NextResponse.json({ status: "success" });
+  } catch (error) {
+    console.error("Error in deleting todo:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
