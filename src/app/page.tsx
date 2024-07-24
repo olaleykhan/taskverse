0c40c304eb3ea1e645c4741980183fa55988a714
lekan.dev <realjoy9@gmail.com>
2024-07-24 17:01:07 +0100

    log user in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import { useState } from "react";
 import TodoList from "@/features/todo/TodoList";
+import Auth from "@/features/auth/Auth";
 
 export default function Home() {
-  return <TodoList />;
+  const [userId, setUserId] = useState<string | undefined>(undefined);
+  if (!userId) return <Auth setUserId={setUserId} />;
+
+  return <TodoList userId={userId} />;
 }
